54de96d061638a18b14b50dc2dfefa724823e272
Rupendra <rupendraparihar90@gmail.com>
2024-08-30 19:48:06 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,21 +11,23 @@ app.use(cors({
 }))
 
 //check for form
-app.use(express.json({
-    limit: '16kb'
-}))
+app.use(express.json({limit: '16kb'}))
 
 //check for URL
-app.use(express.urlencoded({
-    limit: '16kb',
-    extended:true
-}))
+app.use(express.urlencoded({limit: '16kb',extended:true}))
 
 //for store files like pdf,image
 app.use(express.static('public'))
 
 //for cookies
 app.use(cookieParser())
+
+//import Routes
+import userRouter from './routes/user.routes.js'
+app.use('/api/v1/users', userRouter)
+
+
+
 
 
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async function (localfilepath) {
+  try {
+    if (!localfilepath) return null;
+
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localfilepath, {
+      resource_type: "auto",
+    });
+
+    //file has been uploaded now we have to unlink it
+    console.log("file is uploaded on cludinary", response.url);
+    return response;
+
+  } catch (error) {
+    fs.unlinkSync(localfilepath); //remove the locally save temp file as the upload operation got failed
+    return null;
+  }
+};
+
+export {uploadOnCloudinary}
